de1a8258f9f8bd00394202777848b16718fa8061
douglas <douglasichel@hotmail.com>
2022-07-20 22:27:29 -0300

    Desafio-dio-typescript

---
+++ desafio2.ts
@@ -0,0 +1,27 @@
+// Como podemos melhorar o esse código usando TS? 
+interface pessoas  {nome: string, idade: number, profissao: string};
+
+let pessoa1 : pessoas = {
+    nome : "maria",
+    idade : 29,
+    profissao : "atriz"
+};
+
+
+let pessoa2 : pessoas = {
+    nome : "roberto",
+    idade : 19,
+    profissao : "Padeiro"
+}
+
+let pessoa3 : pessoas = {
+    nome: "laura",
+    idade: 32,
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome : "carlos",
+    idade : 19,
+    profissao : "padeiro"
+}
